52a5de34f8df5206c61b8aa001c6d0ab39e38f64
SooKyoung <sksohn01@naver.com>
2023-07-08 13:55:27 +0900

    [Sort] 2751 수 정렬하기 2

---
+++ Sort/sooking87/2751_수정렬하기2.py
@@ -0,0 +1,15 @@
+# 2751 수 정렬하기 2
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+arr.sort()
+
+for i in arr:
+    print(i)
