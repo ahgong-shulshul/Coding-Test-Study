8154c7bcedfdd3ee153a479f56c8c5b7f00c4218
kingwhangzang <soyun011112@sookmyung.ac.kr>
2023-07-09 19:50:08 +0900

    [Greedy] 20115 에너지드링크

--- Greedy/kingwhangzang/20115_에너지드링크.py
+++ Greedy/kingwhangzang/20115_에너지드링크.py
@@ -1,9 +1,9 @@
-N = int(input())
-drinks = list(map(int, input().split()))
-drinks.sort()
+n = int(input())
+drinks = [int(m) for m in input().split()]
+drinks.sort(reverse = True)
 
-total = drinks[-1]
-for i in range(N-1):
-    total += drinks[i]/2
-
+total = drinks[0]
+for i in range(1,n): #첫 번째 드링크는 이미 더했으니 다음 드링크부터
+    total += (drinks[i]/2) #절반씩 더해줌
+    
 print(total)
