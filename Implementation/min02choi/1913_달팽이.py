e0f764ccff358e9ad1a0125048bcc29a831df712
min02choi <min02choi@naver.com>
2023-07-10 16:57:07 +0900

    [Implementation] 10994 별찍기-19

---
+++ Implementation/min02choi/1913_달팽이.py
@@ -0,0 +1,27 @@
+# 1913 달팽이
+# Silver3
+# Implementation 알고리즘
+# 풀이 날짜: 2023-07-10
+
+# 풀이 전략
+
+n = int(input())
+k = int(input())
+
+board = [[0 for _ in range(n)] for _ in range(n)]
+
+num = n**2
+col = 0     # 세로 열
+row = 0     # 가로 행
+status = "d"
+
+# 넣고, 위치 조정
+for i in range(num, 0, -1):
+    board[col][row] = i
+
+
+
+    if (board)
+    
+
+print(board)
