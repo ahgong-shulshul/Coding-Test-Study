bf201718abbd84426f614e7c5784e5ab0f854275
min02choi <min02choi@naver.com>
2023-07-11 21:28:51 +0900

    [Implementation] 1913 달팽이

--- Implementation/min02choi/1913_달팽이.py
+++ Implementation/min02choi/1913_달팽이.py
@@ -5,23 +5,60 @@
 
 # 풀이 전략
 
+
 n = int(input())
 k = int(input())
 
 board = [[0 for _ in range(n)] for _ in range(n)]
 
-num = n**2
-col = 0     # 세로 열
-row = 0     # 가로 행
-status = "d"
+repet = 0
+count = 1
+dir = "u"
+col, row = n // 2, n //2
+till_cnt = -1
 
-# 넣고, 위치 조정
-for i in range(num, 0, -1):
-    board[col][row] = i
+for num in range(1, n ** 2):
+    board[col][row] = num
+    till_cnt += 1
 
+    # 자리 체인지
+    if (till_cnt == count):
+        # 모서리에 도달하였으므로 방향 전환
+        if (dir == "u"):
+            dir = "r"
+        elif (dir == "r"):
+            dir = "d"
+        elif (dir == "d"):
+            dir = "l"
+        elif (dir == "l"):
+            dir = "u"
 
+        repet += 1
 
-    if (board)
+        if (repet == 2):
+            count += 1
+            repet = 0
+
+        till_cnt = 0
     
+    # 그 다음 자리로 이동
+    if (dir == "u"):
+        col -= 1
+    elif (dir == "r"):
+        row += 1
+    elif (dir == "d"):
+        col += 1
+    elif (dir == "l"):
+        row -= 1
 
-print(board)
+board[0][0] = n**2
+
+x, y = 0, 0
+
+for i in range(n):
+    for j in range(n):
+        if (board[i][j] == k):
+            x, y = i, j
+        print(board[i][j], end=" ")
+    print()
+print(x + 1, y + 1)
