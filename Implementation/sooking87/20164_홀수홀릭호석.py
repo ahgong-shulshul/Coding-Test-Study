db49848f37c1456fa08a2b8a0beeed75e5d25f94
SooKyoung <sksohn01@naver.com>
2023-07-13 01:01:43 +0900

    [Implementation] 20164 홀수 홀릭 호석

--- Implementation/sooking87/20164_홀수홀릭호석.py
+++ Implementation/sooking87/20164_홀수홀릭호석.py
@@ -7,69 +7,43 @@
 
 # 514(2) -> 5 + 1 + 4 = 10(1) -> 1(1)
 
-# 쪼개진 상태로 그 값을 유지하면서 사용해야되므로 재귀 사용.
+# 쪼개진 상태로 그 값을 유지하면서 사용해야되므로 재귀를 사용해야될 것 같음.
 
-import sys
 import math
-import copy
-
-def split_three(n):
-    for i in range(1, len(copy_n) - 1):
-        for j in range(i + 1, len(copy_n)):
-            first = int(copy_n[0:i])
-            second = int(copy_n[i:j])
-            last = int(copy_n[j:len(copy_n)])
-            copy_n = str(first + second + last)
-            print(first, second, last)
-            print(copy_n)
 
 
-input = sys.stdin.readline
+def odd_counting(n):
+    odd_cnt = 0
+    for i in n:
+        if int(i) % 2 != 0:
+            odd_cnt += 1
+    return odd_cnt
+
+
+def main_func(n, odd_cnt):
+    global min_v, max_v
+
+    if len(n) == 1:
+        min_v = min(min_v, odd_cnt)
+        max_v = max(max_v, odd_cnt)
+    elif len(n) == 2:
+        first = int(n[0])
+        second = int(n[1])
+        temp = str(first + second)
+        main_func(temp, odd_cnt + odd_counting(temp))
+    else:
+        for i in range(1, len(n) - 1):
+            for j in range(i + 1, len(n)):
+                first = int(n[0:i])
+                second = int(n[i:j])
+                last = int(n[j:len(n)])
+                temp = str(first + second + last)
+                main_func(temp, odd_cnt + odd_counting(temp))
+
+
 n = input()
-# 문자열 입력 시 \n 제거
-n = n.replace('\n', '')
-ans = n
-cnt = 0
-cnt_list = []
-loop = math.comb(len(n) - 1, 2) # 몇 개를 쪼갤 수 있는지
-print(loop)
-for _ in range(loop):
-    copy_n = n
-    while True:
-        print('----------')
-        if int(copy_n) % 2 != 0:
-            cnt += 1
-            print('cnt', cnt)
-        
-        if len(copy_n) == 1:
-            break
-        elif len(copy_n) == 2:
-            print("2인 경우", copy_n)
-            first = int(copy_n[0])
-            second = int(copy_n[1])
-            if first % 2 != 0:
-                cnt += 1
-            if second % 2 != 0:
-                cnt += 1
-            copy_n = str(first + second)
-        else:
-            print('3이상인 경우')
-            for i in range(1, len(copy_n) - 1):
-                for j in range(i + 1, len(copy_n)):
-                    first = int(copy_n[0:i])
-                    second = int(copy_n[i:j])
-                    last = int(copy_n[j:len(copy_n)])
-                    copy_n = str(first + second + last)
-                    print(first, second, last)
-                    print(copy_n)
-                    if first % 2 != 0:
-                        cnt += 1
-                    if second % 2 != 0:
-                        cnt += 1
-                    if last % 2 != 0:
-                        cnt += 1
-                    print('end')
-                print('*')
-            print(copy_n)
-    cnt_list.append(cnt)
-print('cnt', cnt_list)
+min_v = math.inf
+max_v = 0
+
+main_func(n, odd_counting(n))
+print(min_v, max_v)
