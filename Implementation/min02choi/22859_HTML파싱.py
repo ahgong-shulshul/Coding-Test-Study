11565d973c9188787c17f32b2fb944b8807fd1d9
min02choi <min02choi@naver.com>
2023-07-14 17:06:56 +0900

    [Implementation] 22859 HTML 파싱

--- Implementation/min02choi/22859_HTML파싱.py
+++ Implementation/min02choi/22859_HTML파싱.py
@@ -5,9 +5,51 @@
 
 
 html = input()
-first_parse = ""
+html = list(html)
 
-# <p>안의 태그들 다 삭제
-for i in range(len(html)):
-    if (html[i] == "<" and html[i + 1] == "p"):
-        
+sub_tag_open = 0
+idx = 0
+
+# <p>안의 불필요한 태그 삭제
+while ("".join(html[idx:idx + 8]) != "</main>"):
+    if ("".join(html[idx:idx + 3]) == "<p>"):
+        idx += 3
+        while ("".join(html[idx:idx + 4]) != "</p>"):
+            if (html[idx] == "<"):
+                sub_tag_open = 1
+            elif (html[idx] == ">"):
+                sub_tag_open = 0
+                html[idx] = ""
+
+            if (sub_tag_open == 1):
+                html[idx] = ""
+            idx += 1
+    idx += 1
+
+# 두개 이상의 공백 제거
+sentence = "".join(html)
+# html = sentence.replace("  ", " ")
+html = " ".join(sentence.split())
+
+idx = 0
+title = ""
+sent = ""
+# 문장 좌우의 공백 제거
+while ("".join(html[idx:idx + 8]) != "</main>"):
+    if ("".join(html[idx:idx + 4]) == "<div"):
+        idx += 12
+        while (html[idx] != '"'):
+            title += html[idx]
+            idx += 1
+        print("title : " + title)
+        title = ""
+    
+    if ("".join(html[idx:idx + 3]) == "<p>"):
+        idx += 3
+        while ("".join(html[idx:idx + 4]) != "</p>"):
+            sent += html[idx]
+            idx += 1
+        sen = sent.strip()
+        print(sen)
+        sent = ""
+    idx += 1
