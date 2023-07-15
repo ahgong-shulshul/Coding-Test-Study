b0c2b522bc7e6c03baa9be5b8d1cbc271f60b023
[min02choi] <min02choi@naver.com>
2023-07-16 00:17:29 +0900

    [Greedy] 2812 크게만들기

--- Greedy/min02choi/2812_크게만들기.py
+++ Greedy/min02choi/2812_크게만들기.py
@@ -3,10 +3,7 @@
 # Greedy 알고리즘
 # 풀이 날짜: 2023-07-09
 
-# 답은 나오는데 시간초과 뜸. 아마 다른테스트 케이스에 대한 무한루프..
-
 import time
-
 
 n, k = map(int, input().split())
 num = input()
@@ -14,7 +11,7 @@ num = input()
 start = time.time()
 
 # 인덱스 0~k까지의 숫자를 배열에 저장
-can = [num[i] for i in range(k)]
+can = [num[i] for i in range(k + 1)]
 # print(can)
 
 idx = can.index(max(can))       # 가장 큰 수의 인덱스 탐색
@@ -23,22 +20,37 @@ k -= idx
 first = max(can)
 
 num = num[idx:]     # 원본 숫자에서 앞부분 떼어내기
-# print(num)
 
-while(k > 0):
-    can = [num[i] for i in range(1, len(num))]
-    max_num = max(can)
-    for j in range(can.index(max_num)):
+can = [num[i] for i in range(1, len(num))]
+nn = 0
 
-        sub_str = sorted(can[0:can.index(max_num)])
+while(len(can)):
+    cnt = 0
+    for i in can:
+        if (i != ""):
+            cnt += 1
 
-        if (sub_str[j] < max_num and k > 0):
+    if (k == 0 or nn == cnt):
+        break
+
+    temp = sorted(can, reverse=True)
+
+    max_num = temp[nn]
+    for j in range(can.index(max_num, nn)):
+
+        sub_str = sorted(can[0:can.index(max_num, nn)])
+
+        if (sub_str[j] < max_num and k > 0 and sub_str[j] != ""):
             n_idx = can.index(sub_str[j])
             can[n_idx] = ""
             k -= 1
+    nn += 1
 
 print(can)
 print(num)
+
+if (k > 0):
+    can = can[0:len(can) - k]
 
 string = ""
 for i in can:
