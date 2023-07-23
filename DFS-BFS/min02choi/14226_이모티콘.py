93117968c82b9849add588a39f80d8d0a92901bc
[min02choi] <min02choi@naver.com>
2023-07-23 17:49:33 +0900

    [DFS-BFS] 14226 이모티콘

--- DFS-BFS/min02choi/14226_이모티콘.py
+++ DFS-BFS/min02choi/14226_이모티콘.py
@@ -11,36 +11,30 @@ from collections import deque
 
 
 s = int(input())
-
-visited = [[False] * 1000 for i in range(1000)]
+visited = [[False for _ in range(2001)] for _ in range(2001)]
 
 status = deque()
 status.append([1, 0, 0])
 
 while(status):
-    cur = status.popleft()
-    emoji = cur[0]
-    clip = cur[1]
-    time = cur[2]
+    emoji, clip, time = status.popleft()
 
     if (emoji == s):
         print(time)
         break
     
-    if (emoji > 0):
+    if (0 < emoji <= 1000):
         # 복사
         if (visited[emoji][emoji] == False):
             visited[emoji][emoji] = True
             status.append([emoji, emoji, time + 1])
-
         # 삭제
         if (visited[emoji - 1][clip] == False):
             visited[emoji - 1][clip] = True
             status.append([emoji - 1, clip, time + 1])
         
     # 붙여넣기
-    if (clip > 0):
+    if (0 < clip <= 1000 and 0 < emoji + clip <= 2000):
         if (visited[emoji + clip][clip] == False):
             visited[emoji + clip][clip] = True
             status.append([emoji + clip, clip, time + 1])
-
