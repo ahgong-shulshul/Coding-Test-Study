7c068a48eb403c80d89efec768278e3cc2c5ba31
min02choi <min02choi@naver.com>
2023-07-25 21:04:12 +0900

    [Sort] 3151 합이 0

--- Sort/min02choi/3151_합이0.py
+++ Sort/min02choi/3151_합이0.py
@@ -3,11 +3,22 @@
 # 정렬 알고리즘
 # 풀이 날짜: 2023-07-25
 
+# 일반 탐색으로 하면 시간초과
+# 이분탐색 사용할것
+
+import sys
+
 n = int(input())
 std = [int(m) for m in input().split()]
 
 std.sort()
 
+
+for i in range(0, len(std) - 2):
+    for j in range(i + 1, len(std) - 1):
+        
+
+"""
 print(std)
 sum = 0
 cnt = 0
@@ -19,3 +30,4 @@ for i in range(0, len(std) - 2):
                 cnt += 1
 
 print(cnt)
+"""
