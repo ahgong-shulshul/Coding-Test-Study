4f396041b3f0957478b7469638af7fedae76b3b1
sooking87 <sksohn01@naver.com>
2023-07-30 16:25:35 +0900

    [Sort] 1744 수 묶기

--- Sort/sooking87/1744_수묶기.py
+++ Sort/sooking87/1744_수묶기.py
@@ -3,11 +3,9 @@
 # 입력 받은 수를 단 한 번 또는 0번 묶어서 최대 수 출력하기
 
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 n = int(input())
-nums = []
 pos = []
 neg = []
 ans = 0
@@ -16,26 +14,29 @@ for _ in range(n):
     if num > 1:
         pos.append(num)
     elif num == 1:
-        ans += 1  # 1이 입력된다는 것은 1만 더하든,
-
-
-nums.sort(reverse=True)
-queue = deque(nums)
-# print(queue)
-
-num1 = queue.popleft()
-while queue:
-    num2 = queue.popleft()
-    # print(num1, num2)
-    # print(queue)
-    temp1 = num1 * num2
-    temp2 = num1 + num2
-    if temp1 < temp2:
-        ans += num1
-        num1 = num2
+        ans += 1 # 1 1 이렇게 입력이 된다면 밑에서는 무조건 곱하면서 더해지기 때문에 1이 입력되는 경우는 그냥 더해주면 된다.
     else:
-        ans += temp1
-        if queue:
-            num1 = queue.popleft()
-    print(ans)
+        neg.append(num)
+
+pos.sort(reverse=True) # 양수는 내림차순 정렬
+neg.sort() # 음수는 오름차순 정렬
+
+# 양수 더하기
+if len(pos) % 2 == 0:
+    for i in range(0, len(pos), 2):
+        ans += pos[i] * pos[i + 1]
+else:
+    for i in range(0, len(pos) - 1, 2):
+        ans += pos[i] * pos[i + 1]
+    ans += pos[-1]
+
+# 음수 더하기
+if len(neg) % 2 == 0:
+    for i in range(0, len(neg), 2):
+        ans += neg[i] * neg[i + 1]
+else:
+    for i in range(0, len(neg) - 1, 2):
+        ans += neg[i] * neg[i + 1]
+    ans += neg[-1]
+
 print(ans)
