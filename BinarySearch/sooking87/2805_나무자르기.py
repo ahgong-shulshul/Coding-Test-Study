7ef0f4bf7723ca9389c678da60a9c95c7898be82
sooking87 <sksohn01@naver.com>
2023-08-06 16:16:39 +0900

    [BinarySearch] 2805 나무 자르기

--- BinarySearch/sooking87/2805_나무자르기.py
+++ BinarySearch/sooking87/2805_나무자르기.py
@@ -1,6 +1,6 @@
 # 2805 나무 자르기
 
-# 높이 h 지정 -> 모두 잘라 -> 필요한 만큼만 집으로 가져가려고 하는데, 이때 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램
+# 높이 h 지정 - > 모두 잘라 - > 필요한 만큼만 집으로 가져가려고 하는데, 이때 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램
 import sys
 
 input = sys.stdin.readline
@@ -12,21 +12,19 @@ height = [int(i) for i in input().split()]
 
 start = 1
 end = max(height)
-mid = 0
+
 while start <= end:
     mid = (start + end) // 2
-    get = 0
     # 얻은 나무의 길이 구하기
-    for i in range(n):
-        # 나무가 자를 길이보다 짧다면 얻을 수 있는 길이가 없음.
-        if height[i] - mid >= 0:
-            get += height[i] - mid
-    print(get, m)
+    # 나무가 자를 길이보다 짧다면 얻을 수 있는 길이가 없음.
+    get = sum([(t - mid) if t - mid > 0 else 0 for t in height])
+
+    # print(get, m, '자른 길이:', mid)
     if get == m:
         break
     if get < m:
         end = mid - 1
-    else:
+    else :
         start = mid + 1
 
-print(mid)
+print(end)
