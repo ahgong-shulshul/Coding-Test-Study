a3ba7cc8704027fbff25c6224bff151780c93a26
SooKyoung <sksohn01@naver.com>
2023-08-17 00:45:40 +0900

    [DynamicProgramming2] 11660 구간 합 구하기 5

--- DynamicProgramming2/sooking87/11660_구간합구하기5.py
+++ DynamicProgramming2/sooking87/11660_구간합구하기5.py
@@ -8,19 +8,17 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 # m은 합을 구해야되는 횟수
 grid = [[int(i) for i in input().split()] for _ in range(n)]
-pos = []
-for i in range(m):
-    temp = [int(i) for i in input().split()]
-    temp2 = []
-    # 인덱스 0 ~ n - 1까지
-    temp2.append((temp[0] - 1, temp[1] - 1))
-    temp2.append((temp[2] - 1, temp[3] - 1))
-    pos.append(temp2)
 
-for i in range(m):
-    row1, col1 = pos[i][0]
-    row2, col2 = pos[i][1]
-    sum = 0
-    
-    print(sum)
-    print('---------------')
+accum_grid = [[0] * (n+1) for i in range(n+1)]
+# 누적합 구하기
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        accum_grid[i][j] = accum_grid[i][j-1] + \
+            accum_grid[i-1][j] - accum_grid[i-1][j-1] + grid[i-1][j-1]
+        # print(accum_grid)
+
+for k in range(m):
+    row1, col1, row2, col2 = map(int, input().split())
+    result = accum_grid[row2][col2] - accum_grid[row2][col1-1] - \
+        accum_grid[row1-1][col2] + accum_grid[row1-1][col1-1]
+    print(result)
