60f261f97e8a8f7df5344f71ed215c99b8e2bc62
sooking87 <sksohn01@naver.com>
2023-08-17 21:09:43 +0900

    [Temp]

---
+++ DynamicProgramming2/sooking87/2294_동전2.py
@@ -0,0 +1,25 @@
+# 2294 동전 2
+
+# 가치 합이 k원이 되면서 동전의 개수가 최소가 되도록 하려면 동전은 몇 개가 되는지.
+
+import sys
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+value = [int(input()) for i in range(n)]
+
+dp = [100000] * (k+1)
+for i in value:
+    dp[i] = 1
+
+for i in range(min(value)+1, k+1):
+    temp = []
+    temp.append(dp[i])
+    for j in value:
+        if (i-j >= 0) and (dp[i-j] < 100000):
+            temp.append(dp[i-j] + 1)
+            # print(temp)
+    dp[i] = min(temp)
+    # print(dp)
+
+print(dp[k] if dp[k] < 100000 else -1)
