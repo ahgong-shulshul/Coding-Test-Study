898b470bcb39b812759c92e4ce83f7d89e8f1e89
min02choi <min02choi@naver.com>
2023-08-20 16:10:32 +0900

    [DynamicProgramming2] 2225 합 분해

--- DynamicProgramming2/2225_합분해.py
+++ DynamicProgramming2/2225_합분해.py
@@ -12,3 +12,14 @@
 
 
 n, k = map(int, input().split())
+dp = [[0 for _ in range(201)] for _ in range(201)]
+
+for i in range(201):
+    dp[1][i] = 1
+    dp[i][0] = 1
+
+for i in range(1, k + 1):
+    for j in range(1, n + 1):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+print(dp[k][n] % 1000000000)
